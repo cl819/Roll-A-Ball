c0a71752f69726e829b4e1382b4b399c8fa669cc
Courtney Lam <cl819@scarletmail.rutgers.edu>
2016-02-11 13:01:09 -0500

    Roll A Ball Tutorial
    complete

---
+++ Assets/Script/Rotater.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (new Vector3 (15, 0, 45) * Time.deltaTime);
+	}
+}
